bdb175840e910592bc6a5e39714682caae32b065
Dominik Drozd <dominik_drozd@vp.pl>
2021-11-01 14:05:44 +0100

    project config

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const hbs  = require('express-handlebars');
+const { clientRouter } = require('./routers/client');
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.static('public'));
+app.use(express.json());
+app.engine('.hbs', hbs({
+    extname: '.hbs',
+    // helpers: handlebarsHelpers,
+}));
+app.set('view engine', '.hbs');
+
+app.use('/client', clientRouter);
+
+app.listen('3000', 'localhost');
